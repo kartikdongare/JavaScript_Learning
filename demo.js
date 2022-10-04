816d6610c74d23bb1a2497b276e8e560d0bd3b39
kartikdongare <kartikdongare2000@gmail.com>
2022-10-04 13:04:34 +0530

    First Program execution in js
    First Program execution in js

---
+++ demo.js
@@ -0,0 +1,14 @@
+console.log("Kartik Martand Dongare");
+console.log("My hobbies are following:-");
+console.log("1.Watching Movies/News");
+console.log("2.Listening Music");
+console.info("My strength are");
+console.info("1.i capable to handle to difficult situation");
+console.info("2.i am not easily distract.")
+console.info("3.i am a hard worker person");
+console.warn("my weakness are");
+console.warn("1sleeping");
+console.warn("2.i don't control anger ,who is lying with me");
+console.warn("3.i did not forgot easily who is shouting with me any without reason");
+console.log(30+60);
+console.log(49*479);
