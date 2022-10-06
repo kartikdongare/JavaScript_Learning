d4f3cc0a8a134a76d67830ec60f893fdc68d4280
kartikdongare <kartikdongare2000@gmail.com>
2022-10-06 15:37:17 +0530

    First assignment on Variable
    First assignment on Variable

---
+++ 01_variable_assign.js
@@ -0,0 +1,18 @@
+//first Question
+var myFullName;
+console.log(myFullName);
+myFullName="kartik dongare";
+console.log(myFullName);
+myFullName="kartik martand dongare";
+console.log(myFullName);
+//seconfd question
+var myLove="I love only JS";
+console.log(myLove);
+//third question
+var firstName="kartik",lastName="donagre",age=22,address="Sangola Tal=sangola Dist-solapur",pin_no=413308,gender="male",is_married="single";
+console.log("First Name:-"+firstName);
+console.log("Last Name:-"+lastName);
+console.log("Address:-"+address);
+console.log("Pin code:-"+pin_no);
+console.log("Gender:-"+gender);
+console.log("Is married:-"+is_married);
