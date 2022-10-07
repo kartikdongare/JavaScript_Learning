036db445301ff7e4d5e3ef729cb68f94985707e6
kartikdongare <kartikdongare2000@gmail.com>
2022-10-07 11:54:27 +0530

    assignment on datatype
    assignment on datatype

---
+++ 02_datatype_assign1.js
@@ -0,0 +1,4 @@
+var my_number=2511,my_string="kartik",bool=false;
+console.log("My_number is:-",my_number,"datatype is:-",typeof(my_number));
+console.log("my_string is:-",my_string,"datatype is:-",typeof(my_string));
+console.log("boolean is:-",bool,"datatype is:-",typeof(bool));
