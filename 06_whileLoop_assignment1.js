109b1362e4442deae0134d95ff0a9e470a4e5009
kartikdongare <kartikdongare2000@gmail.com>
2022-10-14 13:53:36 +0530

    while loop assignment1
    while loop assignment1

---
+++ 06_whileLoop_assignment1.js
@@ -0,0 +1,61 @@
+//WAP to print number from 5 to 15 by increamenting 1
+console.log("-------------------1st---------------------");
+var num=5;
+while (num<=15) {
+    console.log(num);
+    num+=1;
+}
+
+//WAP to print number from 50 to 40 by decreamenting 1
+console.log("------------------2st-------------------");
+var num=50;
+while (num>=40) {
+    console.log(num);
+    num-=1;
+}
+
+//WAP to find first 15 odd nummber
+console.log("-------------3rd-------------------");
+var num=1;
+console.log("first 15 odd numbers following:-");
+while (num<=30) {
+    if(num%2==1){
+        console.log(num);
+    }
+    num+=1;
+}
+
+//WAP to find first 10 even nummber
+console.log("-------------4rd-------------------");
+var num=0;
+console.log("first 10 even numbers following:-");
+while (num<=20) {
+    if(num%2==0){
+        console.log(num);
+    }
+    num+=1;
+}
+
+//WAP to print table like 5,10,15,20------50
+console.log("----------------5th---------------------");
+var num=5;
+while (num<=50) {
+    console.log(num);
+    num+=5;
+}
+
+//WAP to print table 10 like-->10,20,30-----100
+console.log("-------------6th-------------------");
+var num=10;
+while (num<=100) {
+    console.log(num);
+    num+=10;
+}
+
+//WAP to print reverse table 100 like--100,90,80,-----10
+console.log("-----------------7th-------------------");
+var num=100;
+while (num>=10) {
+    console.log(num);
+    num=num-10;
+}
