e36699a39df7e607269c3a174f9072be96a4916f
kartikdongare <kartikdongare2000@gmail.com>
2022-11-06 17:25:08 +0530

    swaping number
    swaping number

---
+++ 01_swap_variable.js
@@ -0,0 +1,12 @@
+var my_number=100;
+var your_number=200;
+console.log("before swapping");
+console.log(my_number);
+console.log(your_number);
+
+var temp;
+temp=my_number;
+my_number=your_number;
+your_number=temp;
+console.log("after the swapping");
+console.log(my_number,your_number);
