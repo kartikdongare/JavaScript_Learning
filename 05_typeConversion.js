8d689c9780e4aa93d8caeec280b990e103bdd951
kartikdongare <kartikdongare2000@gmail.com>
2022-11-13 20:33:15 +0530

    Type Conversion

---
+++ 05_typeConversion.js
@@ -0,0 +1,7 @@
+console.log("===== Implicit conversion======");
+console.log("4"-true); // 4-1
+
+var operand2 = 200;
+console.log(operand2, typeof operand2);
+var operand2Str =  operand2.toString();
+console.log(operand2Str, typeof operand2Str);
