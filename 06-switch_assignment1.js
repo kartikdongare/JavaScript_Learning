7807cc0745fa93882721456ca009ec29a0a9c72e
kartikdongare <kartikdongare2000@gmail.com>
2022-11-14 15:26:08 +0530

    Switch statement assignment

---
+++ 06-switch_assignment1.js
@@ -0,0 +1,45 @@
+var result = function (value) {
+    switch (value) {
+        case 1:
+            console.log("January");
+            break;
+        case 2:
+            console.log("February");
+            break;
+        case 3:
+            console.log("march");
+            break;
+        case 4:
+            console.log("April");
+            break;
+        case 5:
+            console.log("may");
+            break;
+        case 6:
+            console.log("june");
+            break;
+        case 7:
+            console.log("july");
+            break;
+        case 8:
+            console.log("Aguast");
+            break;
+        case 9:
+            console.log("September");
+            break;
+        case 10:
+            console.log("Octomber");
+            break;
+        case 11:
+            console.log("November");
+            break;
+        case 12:
+            console.log("December");
+            break;
+
+        default:
+            console.log("invalid value");
+            break;
+    }
+}
+result(3);
