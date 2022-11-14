b0c9e0e014fcc8fa1dfb96787f4dbb5efbd053bb
kartikdongare <kartikdongare2000@gmail.com>
2022-11-14 15:27:30 +0530

    Switch case

---
+++ 06_switch_case.js
@@ -0,0 +1,28 @@
+var dayNumber = 2;
+switch (dayNumber) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednrsday");
+        break;
+    case 4:
+        console.log("thirsday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+
+    default:
+        console.log("Ivalid input");
+        break;
+}
